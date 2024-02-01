374ca47e1ad4b70da000f46463c343b0417661d6
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-02-01 16:52:51 -0300

    feat: setup fastify

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import Fastify from "fastify";
+import { env } from "./utils/env";
+
+const fastify = Fastify({ logger: true });
+
+fastify.get("/", async () => {
+    return { hello: "world" };
+});
+
+try {
+    await fastify.listen({ port: env.PORT });
+    console.log(`Server listening on port ${env.PORT}`);
+}
+catch (err) {
+    console.error(err);
+    process.exit(1);
+}
